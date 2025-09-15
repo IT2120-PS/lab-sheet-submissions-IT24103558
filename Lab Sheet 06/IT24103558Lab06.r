4b43b72b246d8140072057e851ab87d84b8a21f0
IT24103558 <it24103558@my.sliit.lk>
2025-09-15 18:13:48 +0530

    lab 6

---
+++ Lab Sheet 06/IT24103558Lab06.r
@@ -0,0 +1,19 @@
+getwd()
+setwd("C:\\Users\\Methasa\\Desktop\\IT24103558")
+##Question 1
+#Part 1
+#Binomial Distribution
+#Here, random variable X has binomial distribution with n=47 and p=0.85
+
+dbinom(47,50,0.85)
+
+##Question 02
+#Part 1
+# A call center receives an average of 12 customer calls per hour
+
+#Part 2
+#Poisson distribution
+#Here, random variable X has poisson distribution with lambda=12
+
+dpois(15,12)
+
