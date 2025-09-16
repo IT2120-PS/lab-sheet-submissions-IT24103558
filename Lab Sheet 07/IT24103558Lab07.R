293adcbe6d3f65d96cc849f12a549bdbe179c7ce
IT24103558 <it24103558@my.sliit.lk>
2025-09-16 19:40:27 +0530

    lab 7

---
+++ Lab Sheet 07/IT24103558Lab07.R
@@ -0,0 +1,18 @@
+getwd()
+setwd("C:\\Users\\Methasa\\Desktop\\IT24103558")
+
+#Q1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+#Q2
+pexp(2, rate=1/3)
+
+#Q3
+#part 1
+1 - pnorm(130, mean=100, sd=15)
+
+pnorm(130, mean=100, sd=15, lower.tail = FALSE)
+
+#part 2
+qnorm(0.95, mean=100, sd=15)
+
